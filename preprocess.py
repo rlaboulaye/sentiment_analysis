3bd03d5f8b77399277551c34b531f47c7a1a99a6
Roland Laboulaye <rlaboulaye@gmail.com>
2018-03-12 17:19:05 -0600

    added back stemming with mode as embedding

--- preprocess.py
+++ preprocess.py
@@ -22,13 +22,28 @@ def preprocess(documents):
 def preprocess_tweets(documents, vocabulary):
 	tokenizer = RegexpTokenizer(r'\w+\'[a-z]+|\w+')
 	en_stop = get_stop_words('en')
+	p_stemmer = PorterStemmer()
 
+	stem_to_possible_words = {}
+	stem_to_word = {}
 	texts = []
 	for document in documents:
 		clean_document = tweet_preprocessor.clean(document)
 		clean_lower_document = clean_document.lower()
 		tokens = tokenizer.tokenize(clean_lower_document)
-		stopped_tokens = [i for i in tokens if not i in en_stop]
+		stopped_tokens = [token for token in tokens if not token in en_stop]
 		valid_tokens = [token for token in stopped_tokens if token in vocabulary]
-		texts.append(tokens)
+		stemmed_tokens = []
+		for token in valid_tokens:
+			stem = p_stemmer.stem(token)
+			if stem not in stem_to_possible_words:
+				stem_to_possible_words[stem] = []
+			stem_to_possible_words[stem].append(token)
+			stemmed_tokens.append(stem)
+		if len(stemmed_tokens) > 0:
+			texts.append(stemmed_tokens)
+	for stem in stem_to_possible_words:
+		possible_words = stem_to_possible_words[stem]
+		stem_to_word[stem] = max(set(possible_words), key=possible_words.count)
+	texts = [[stem_to_word[token] for token in text] for text in texts]
 	return texts
